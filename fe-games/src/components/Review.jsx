e78f94f70b532ebb496371bb6c78e734267d5630
Tyr <77638286+tyr-ros@users.noreply.github.com>
2023-06-01 10:49:29 +0100

    responsive

--- fe-games/src/components/Review.jsx
+++ fe-games/src/components/Review.jsx
@@ -5,36 +5,36 @@ import { useParams } from "react-router-dom";
 
 export function Review() {
   const [currentReview, setCurrentReview] = useState([]);
-  const [isLoading, setLoading] = useState(true)
+  const [isLoading, setLoading] = useState(true);
 
   const { review_id } = useParams();
 
-  console.log(review_id);
-
   useEffect(() => {
     fetchReviewById(review_id).then(({ review }) => {
-        setLoading(false)
+      setLoading(false);
       setCurrentReview(review);
     });
   }, [review_id]);
 
-  if (isLoading){
-    return <span className="loader"></span>
+  if (isLoading) {
+    return <span className="loader"></span>;
   }
   return (
-    <main>
-      <img
-        className="img-container"
-        src={currentReview.review_img_url}
-        alt={currentReview.title}
-      />
-      <p>{currentReview.title}</p>
-      <p>{currentReview.category}</p>
-      <p>{currentReview.designer}</p>
-      <p>{currentReview.owner}</p>
-      <p>{currentReview.review_body}</p>
-      <p>{currentReview.votes}</p>
-      <p>{dateConverter(currentReview.created_at)}</p>
+    <main className="review_list">
+      <section className="review">
+        <img
+          className="img-container"
+          src={currentReview.review_img_url}
+          alt={currentReview.title}
+        />
+        <h1>{currentReview.title}</h1>
+        <p>{currentReview.category}</p>
+        <p>Designed by {currentReview.designer}</p>
+        <p>{currentReview.owner}</p>
+        <p>{currentReview.review_body}</p>
+        <p>Upvotes are currently {currentReview.votes}</p>
+        <p>{dateConverter(currentReview.created_at)}</p>
+      </section>
     </main>
   );
 }
