5e68e1e569be82a8a4b7c3d3c59982984e8af4d3
Tyr <77638286+tyr-ros@users.noreply.github.com>
2023-06-01 16:03:19 +0100

    6-comments-by-review

--- fe-games/src/components/Review.jsx
+++ fe-games/src/components/Review.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
-import { fetchReviewById } from "./api";
+import { fetchReviewById, fetchCommentsByReviewId } from "./api";
 import { dateConverter } from "./utils";
 import { useParams } from "react-router-dom";
 
 export function Review() {
   const [currentReview, setCurrentReview] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [currentComments, setCurrentComments] = useState([]);
 
   const { review_id } = useParams();
 
@@ -13,6 +14,9 @@ export function Review() {
     fetchReviewById(review_id).then(({ review }) => {
       setLoading(false);
       setCurrentReview(review);
+    });
+    fetchCommentsByReviewId(review_id).then(({ comments }) => {
+      setCurrentComments(comments);
     });
   }, [review_id]);
 
@@ -34,6 +38,18 @@ export function Review() {
         <p>{currentReview.review_body}</p>
         <p>Upvotes are currently {currentReview.votes}</p>
         <p>{dateConverter(currentReview.created_at)}</p>
+        <ul className="comment_list">
+          {currentComments.map((comment) => {
+            return (
+              <li key={comment.comment_id} className="comment">
+                <p className="comment_body">{comment.body}</p>
+                <p className="comment_author">by {comment.author}</p>
+                <p className="comment_votes">Votes: {comment.votes}</p>
+                <p className="comment_created_at">Posted on {dateConverter(comment.created_at)}</p>
+              </li>
+            );
+          })}
+        </ul>
       </section>
     </main>
   );
